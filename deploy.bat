2dbf4b09e464a2076cb8bd8b481e5ef1bac08f17
dallben <dallben@gmail.com>
2023-06-07 18:43:36 -0400

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -5,6 +5,6 @@ rmdir dist\install /S /Q
 mkdir dist\install
 call npm install --prefix dist\install .
 del dist\dist.zip
-7z a dist\dist.zip .\dist\install\node_modules\alexa-plex\*
+7z a dist\dist.zip .\dist\install\node_modules\alexa-plex-es\*
 echo Function zipped. Updating...
-aws lambda update-function-code --zip-file fileb://dist\dist.zip --function-name alexa-plex
+aws lambda update-function-code --zip-file fileb://dist\dist.zip --function-name alexa-plex-es
